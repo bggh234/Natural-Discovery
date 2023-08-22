0fd7a4473632ecd610e2758ee0b5a31caeada434
bggh234 <bggh234@outlook.com>
2023-08-22 11:13:07 +0800

    Set up main files.

--- src/main/java/com/bggh234/natural_discovery/natural_discovery/NaturalDiscovery.java
+++ src/main/java/com/bggh234/natural_discovery/natural_discovery/NaturalDiscovery.java
@@ -1,10 +1,18 @@
 package com.bggh234.natural_discovery.natural_discovery;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NaturalDiscovery implements ModInitializer {
+    //References
+    public static final String MOD_ID = "natural_discovery";
+
+    // Mod Logger
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Hello explorer!");
     }
 }
--- src/main/java/com/bggh234/natural_discovery/natural_discovery/client/NaturalDiscoveryClient.java
+++ src/main/java/com/bggh234/natural_discovery/natural_discovery/client/NaturalDiscoveryClient.java
@@ -1,2 +1,10 @@
-public class NaturalDiscoveryClient {
+package com.bggh234.natural_discovery.natural_discovery.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NaturalDiscoveryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
