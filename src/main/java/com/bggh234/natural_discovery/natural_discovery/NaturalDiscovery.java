8b5304f358a968109ed7f44b1737f454c044c152
bggh234 <bggh234@outlook.com>
2023-08-22 10:57:42 +0800

    Upload the original project。

---
+++ src/main/java/com/bggh234/natural_discovery/natural_discovery/NaturalDiscovery.java
@@ -0,0 +1,10 @@
+package com.bggh234.natural_discovery.natural_discovery;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NaturalDiscovery implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
